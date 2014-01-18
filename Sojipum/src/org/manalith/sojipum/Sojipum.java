0f63c523541eebb55f02e561811fdf3387136762
mmx900 <mmx900@gmail.com>
2014-01-18 14:54:50 +0900

    코드 정리

--- Sojipum/src/org/manalith/sojipum/Sojipum.java
+++ Sojipum/src/org/manalith/sojipum/Sojipum.java
@@ -5,8 +5,6 @@ import android.app.Application;
 public class Sojipum extends Application {
 	@Override
 	public void onCreate() {
-		//DatabaseHelper.getInstance(this).getWritableDatabase();
-
 		super.onCreate();
 	}
 }
